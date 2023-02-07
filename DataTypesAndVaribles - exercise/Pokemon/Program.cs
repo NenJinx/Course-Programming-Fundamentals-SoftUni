2cbc9de7adaeb34f94dc1c1cad5f39ac14a56bc8
NenJinx <mr.yordangeorgiev@gmail.com>
2023-02-07 19:37:40 +0200

    Add two tasks

---
+++ DataTypesAndVaribles - exercise/Pokemon/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                int pokePower, distance, countTargets = 0, y;
+                double halfPower = 0;
+                pokePower = int.Parse(Console.ReadLine());
+                distance = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+                halfPower = (double)pokePower / 2;
+                while (pokePower >= distance)
+                {
+                    pokePower = pokePower - distance;
+                    countTargets++;
+                    if (pokePower == halfPower)
+                    {
+                        pokePower = pokePower / y;
+                    }
+                }
+                Console.WriteLine(pokePower);
+                Console.WriteLine(countTargets);
+            }
+        }
+    }
+
+}
+    
+
---
+++ DataTypesAndVaribles - exercise/SpiceMustFlow/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiceMustFlowTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int startingYield = int.Parse(Console.ReadLine());
+            const int consumeSpices = 26;
+            const int yieldDrops = 10;
+            int extractSpices = 0;
+            int workerDays = 0;
+            while (startingYield >= 100)
+            {
+                extractSpices += startingYield - consumeSpices;
+                workerDays += 1;
+                startingYield -= yieldDrops;
+            }
+            if (extractSpices < 26)
+            {
+                extractSpices = 0;
+            }
+            else 
+            {
+                extractSpices -= consumeSpices;
+            }
+            Console.WriteLine(workerDays);
+            Console.WriteLine(extractSpices);
+        }
+    }
+}
