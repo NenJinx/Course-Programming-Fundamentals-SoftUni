844750f7ba0628739728448fa5d8627c816fe7b1
NenJinx <mr.yordangeorgiev@gmail.com>
2023-02-25 19:31:07 +0200

    Add one task

---
+++ List/Train/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagonsCount = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            int maxCappacity = int.Parse(Console.ReadLine());
+
+            string command = string.Empty;
+            string[] separator = { " " };
+            while ((command = Console.ReadLine()) != "end")
+            {
+                List<string> cmdArgs = command.Split(separator, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (cmdArgs[0] == "Add")
+                {
+                    wagonsCount.Add(int.Parse(cmdArgs[1]));
+                }
+                else
+                {
+
+                    for (int j = 0; j < wagonsCount.Count; j++)
+                    {
+                        if (wagonsCount[j] + int.Parse(cmdArgs[0]) <= maxCappacity)
+                        {
+                            wagonsCount[j] += int.Parse(cmdArgs[0]);
+                            break;
+                        }
+                    }
+                }
+            }
+            printWagonsCount(wagonsCount);
+        }
+
+
+        static void printWagonsCount(List<int> wagonsCount)
+        {
+            foreach (int numbers in wagonsCount)
+            {
+                Console.Write(numbers + " ");
+            }
+        }
+    }
+}
+
+
