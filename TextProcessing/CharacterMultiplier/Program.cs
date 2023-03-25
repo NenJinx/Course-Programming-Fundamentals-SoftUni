708b7331c9875234650aaf755507bc6809552ac2
NenJinx <mr.yordangeorgiev@gmail.com>
2023-03-25 19:07:49 +0200

    Add two tasks

---
+++ TextProcessing/CharacterMultiplier/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterMultiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] textValues = Console.ReadLine().Split(' ');
+            string text = textValues[0];
+            string textOne = textValues[1];
+            Console.WriteLine(SymbolsCodeSum(text, textOne));
+        }
+        static int SymbolsCodeSum(string firstText, string secondText)
+        {
+            int sum = 0;
+            int shorter = 0;
+            string longer = string.Empty;
+            char[] symbs = new char[firstText.Length];
+            symbs = firstText.ToCharArray();
+            char[] symbsSecond = new char[secondText.Length];
+            symbs = secondText.ToCharArray();
+            if (firstText.Length < secondText.Length)
+            {
+                shorter = firstText.Length;
+                longer = secondText;
+            }
+            else if (secondText.Length < firstText.Length)
+            {
+                shorter = secondText.Length;
+                longer = firstText;
+            }
+            else
+            {
+                shorter = firstText.Length;
+            }
+            for (int i = 0; i < shorter; i++)
+            {
+                sum += firstText[i] * secondText[i];
+            }
+            for (int i = shorter; i < longer.Length; i++)
+            {
+                sum += longer[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ TextProcessing/ExtractFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] separator = { "\\" };
+            String[] separatorOne = { "." };
+            string filePath = Console.ReadLine();
+            int lastIndexWithSlash = filePath.LastIndexOf('\\');
+            //string substr = filePath.Substring(lastIndexWithSlash + 1, filePath.Length- 1 - lastIndexWithSlash);
+            string[] substr = filePath.Substring(lastIndexWithSlash + 1, filePath.Length - 1 - lastIndexWithSlash).
+                Split(separatorOne,StringSplitOptions.RemoveEmptyEntries);
+            string fileName = substr[0];
+            string fileExtension = substr[1];
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
+
+
+
