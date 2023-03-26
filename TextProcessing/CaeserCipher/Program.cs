013d2e4b7ffa06e5a69afe52b33cc554f18a0202
NenJinx <mr.yordangeorgiev@gmail.com>
2023-03-26 16:20:33 +0300

    Add one

---
+++ TextProcessing/CaeserCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaeserCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] chars = text.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                int symb = chars[i] + 3;
+                chars[i] = (char)symb;
+            }
+            Console.WriteLine(chars);
+        }
+    }
+}
