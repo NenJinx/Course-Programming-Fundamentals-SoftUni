8681bd64cbf463c76e2f0f54b530e387fedc1fa1
NenJinx <mr.yordangeorgiev@gmail.com>
2023-03-31 20:22:28 +0300

    Add

---
+++ RegularExpressions/Furniture/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            string pattern = @">>(?<name>[A-z]+)<<(?<price>\d+\.?\d*)!(?<quantity>\d+)";
+            Regex regex = new Regex(pattern);
+            double productsCost = 0;
+            List <string>names = new List<string>();
+            while ((command = Console.ReadLine()) != "Purchase")
+            {
+                Match match = regex.Match(command);
+                if (match.Success)
+                {
+                    productsCost += double.Parse(match.Groups["price"].Value) * double.Parse(match.Groups["quantity"].Value);
+                    names.Add(match.Groups["name"].Value);
+                }
+            }
+            Console.WriteLine("Bought furniture:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine($"Total money spend: {productsCost}");
+        }
+    }
+}
