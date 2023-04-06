643f4900333b27ada0b518fe14bf704de66a77a7
NenJinx <mr.yordangeorgiev@gmail.com>
2023-04-06 19:22:48 +0300

    Add

---
+++ FinalExamRetake/AdAstra/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"(\||#)(?<product>[A-Z][a-z]+ ?[A-Z]?[a-z]+)\1(?<date>[0-9]{2}/[0-9]{2}/[0-9]{2})\1(?<calories>[0-9]+)\1");
+            MatchCollection matches = regex.Matches(input);
+            int totalCalories = 0;
+            foreach (Match match in matches)
+            {
+                totalCalories += int.Parse(match.Groups["calories"].Value);
+            }
+            int days = totalCalories / 2000;
+            Console.WriteLine($"You have food to last you for: {days} days!");
+            foreach (Match item in matches)
+            {
+                Console.WriteLine($"Item: {item.Groups["product"].Value}, Best before: {item.Groups["date"].Value}, Nutrition: {item.Groups["calories"].Value}");
+            }
+        }
+    }
+}
---
+++ FinalExamRetake/TheImitationGame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheImitationGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "Decode")
+            {
+                string[] cmdArgs = command.Split('|');
+                string commandName = cmdArgs[0];
+                if (commandName == "Move")
+                {
+                    int number = int.Parse(cmdArgs[1]);
+                    string substr = message.Substring(0, number);
+                    message = message.Insert(message.Length, substr);
+                    message = message.Remove(0, number);
+                }
+                else if (commandName == "Insert")
+                {
+                    int index = int.Parse(cmdArgs[1]);
+                    string value = cmdArgs[2];
+                    message = message.Insert(index, value);
+                }
+                else if (commandName == "ChangeAll")
+                {
+                    string substring = cmdArgs[1];
+                    string replacement = cmdArgs[2];
+                    if (message.Contains(substring))
+                    {
+                        message = message.Replace(substring, replacement);
+                    }
+                }
+            }
+            Console.WriteLine("The decrypted message is: " + message);
+        }
+    }
+}
+
+
+
