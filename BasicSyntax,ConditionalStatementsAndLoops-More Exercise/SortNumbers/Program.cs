4b44f87b20b93dd42fdef1d76862307bd989e72b
NenJinx <122741133+NenJinx@users.noreply.github.com>
2023-09-06 15:01:31 +0300

    Basic Syntax, Conditional Statements and Loops solutions

--- BasicSyntax,ConditionalStatementsAndLoops-More Exercise/SortNumbers/Program.cs
+++ BasicSyntax,ConditionalStatementsAndLoops-More Exercise/SortNumbers/Program.cs
@@ -1,17 +1,10 @@
-﻿using Microsoft.Win32.SafeHandles;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SortNumbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             int firstNum = int.Parse(Console.ReadLine());
             int secondNum = int.Parse(Console.ReadLine());
             int thirdNum = int.Parse(Console.ReadLine());
@@ -22,7 +15,6 @@ namespace SortNumbers
             Console.WriteLine(maxNum);
             Console.WriteLine(middleNum);
             Console.WriteLine(minNum);
-
         }
     }
 }
